c34e21530b7bd4c8ee07757e3894d66d4142d709
Adam <a_prusse@hotmail.com>
2018-01-29 14:37:19 -0700

    variables in typescript

---
+++ 006_variables.js
@@ -0,0 +1,15 @@
+// var fullName : string = "Adam Prusse";
+// // ^ Declaring      ^ Assignment
+// let paidAccount : Boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Jimmy Neutron";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Adam", "Prusse");
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,18 @@
+// var fullName : string = "Adam Prusse";
+// // ^ Declaring      ^ Assignment
+// let paidAccount : Boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Jimmy Neutron";
+// paidAccount = false;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f, l) {
+	var greeting : string = "Hi there, ";
+	console.log(greeting + f + " " + l)
+}
+
+printName("Adam", "Prusse")
