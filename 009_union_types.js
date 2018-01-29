b818d007b32f2c473ffca5e6791467457254eec8
Adam <a_prusse@hotmail.com>
2018-01-29 15:39:07 -0700

    Added union types

---
+++ 009_union_types.js
@@ -0,0 +1,10 @@
+var players = ["Altuve", "Corerra", "Bregman"];
+var playerNumbers = [25, 3, 2];
+console.log(players);
+console.log(playerNumbers);
+var names;
+names = ["Adam Prusse", "Jimmy Neutron"];
+console.log(names);
+names = "carol";
+console.log(names);
+//# sourceMappingURL=009_union_types.js.map
---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArray = Array<string|number>;
+let players : PlayerArray = ["Altuve", "Corerra", "Bregman"];
+let playerNumbers : PlayerArray = [25, 3, 2];
+console.log(players);
+console.log(playerNumbers);
+
+var names : string[]|string;
+names = ["Adam Prusse", "Jimmy Neutron"];
+console.log(names);
+names = "carol";
+console.log(names);
